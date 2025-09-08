cc43a851342a593e4ce93bdcf529fd9157accb21
Milos Barjaktarovic <bmilos1983@gmail.com>
2025-09-09 00:09:11 +0200

    Dodan SQL fajl za cas_11

---
+++ cas_11/prvi_cas.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2025 at 11:55 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prvi_cas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proizvodi`
+--
+
+CREATE TABLE `proizvodi` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(100) DEFAULT NULL,
+  `opis` text DEFAULT NULL,
+  `cena` decimal(10,2) DEFAULT NULL,
+  `dan_nabavke` date DEFAULT NULL,
+  `kolicina` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `proizvodi`
+--
+ALTER TABLE `proizvodi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `proizvodi`
+--
+ALTER TABLE `proizvodi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
